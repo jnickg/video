487ef9a6c9040ab7df65b98870389733b9155156
jnickg <me@jnickg.net>
2024-01-07 22:03:52 -0800

    Initial commit. Add first script

---
+++ transcode_for_davinci.sh
@@ -0,0 +1,3 @@
+for i in *.mkv; do                                                                                                                           ✔ 
+        ffmpeg -i "$i" -vcodec mjpeg -q:v 2 -acodec pcm_s16be -q:a 0 -f mov "${i%.*}.mov";
+done
